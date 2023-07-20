78858e08784b95fbd004522ec7bf63058314da48
Christopher Bernhard <chris_bernhard@yahoo.com>
2023-07-20 12:54:21 +0000

    working on hover effects

--- js/generateProjects.js
+++ js/generateProjects.js
@@ -9,18 +9,23 @@ export async function generateProjects() {
 
         let projectsHtml = projects.map((project) => {
             return `
-                <div class="project-card">
-                    <img src="${project.thumbnail}" alt="${project.title}" />
+                <div class="projects-card">
+                    <h2 class="projects-card-title">${project.title}</h2>
+                    <img src="${project.thumbnail}" alt="${project.title}" class="projects-card-img" />
                 </div>
             `
         }).join('')
 
         projectsGrid.innerHTML = projectsHtml;
+
+        let projectsCardImgs = document.querySelectorAll('.projects-card-img');
+        let projectsCardTitle = document.querySelectorAll('.projects.card-title');
+        let projectsCard = document.querySelectorAll('.projects-card');
+
         
     } catch (error) {
         console.log(error)
     }
-
 
     
 }
