3fc30e42a8a91781854824d80b4bcff5e1e16097
Christopher Bernhard <chris_bernhard@yahoo.com>
2023-07-21 12:48:37 +0000

    working on projects rollover

--- js/generateProjects.js
+++ js/generateProjects.js
@@ -9,20 +9,20 @@ export async function generateProjects() {
 
         let projectsHtml = projects.map((project) => {
             return `
-                <div class="projects-card">
-                    <h2 class="projects-card-title">${project.title}</h2>
-                    <img src="${project.thumbnail}" alt="${project.title}" class="projects-card-img" />
-                </div>
+                <a href="#">
+                    <div class="projects-card">
+                        <img class="projects-card-img" src="${project.thumbnail}" alt="${project.title}" />    
+                        <div class="projects-card-overlay">
+                            <h2 class="projects-card-title">${project.title}</h2>
+                            <p class="projects-card-text">${project.description}</p>
+                        </div>
+                    </div>                
+                </a>
             `
         }).join('')
 
         projectsGrid.innerHTML = projectsHtml;
 
-        let projectsCardImgs = document.querySelectorAll('.projects-card-img');
-        let projectsCardTitle = document.querySelectorAll('.projects.card-title');
-        let projectsCard = document.querySelectorAll('.projects-card');
-
-        
     } catch (error) {
         console.log(error)
     }
