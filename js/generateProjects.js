f25283428d8f2ae4df25392ccc4f6510ec4de2f6
Christopher Bernhard <chris_bernhard@yahoo.com>
2023-07-23 14:41:57 +0000

    working on hover animation

--- js/generateProjects.js
+++ js/generateProjects.js
@@ -11,11 +11,17 @@ export async function generateProjects() {
             return `
                 <a href="#">
                     <div class="projects-card">
+                        <div class="hover-animation">
+                        
+                        
                         <img class="projects-card-img" src="${project.thumbnail}" alt="${project.title}" />    
                         <div class="projects-card-overlay">
                             <h2 class="projects-card-title">${project.title}</h2>
                             <p class="projects-card-text">${project.description}</p>
                         </div>
+                        
+                        </div>
+
                     </div>                
                 </a>
             `
